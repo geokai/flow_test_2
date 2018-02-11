9bf2b4d41d1699a3f1e3fb24619f6d6404cb3f3b
George Kaimakis <geoptus@gmail.com>
2018-02-11 23:06:14 +0000

    adds fullname method and prints emp names

--- oop.py
+++ oop.py
@@ -9,6 +9,8 @@ class Employee:
         self.pay = pay
         self.email = first + '.' + last + '@company.com'
 
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
 
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'User', 60000)
@@ -16,5 +18,7 @@ emp_2 = Employee('Test', 'User', 60000)
 # print(emp_1)
 # print(emp_2)
 
+print(emp_1.fullname())
+print(emp_2.fullname())
 print(emp_1.email)
 print(emp_2.email)
