963765d7733cc36c604df41550ae8132948184c9
George Kaimakis <geoptus@gmail.com>
2018-02-11 22:57:23 +0000

    adds init method and prints emails

--- oop.py
+++ oop.py
@@ -2,24 +2,19 @@
 
 
 class Employee:
-    pass
+    
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
 
 
-emp_1 = Employee()
-emp_2 = Employee()
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'User', 60000)
 
-print(emp_1)
-print(emp_2)
-
-emp_1.first = 'Corey'
-emp_1.last = 'Schafer'
-emp_1.email = 'Corey.Schafer@company.com'
-emp_1.pay = 50000
-
-emp_2.first = 'Test'
-emp_2.last = 'User'
-emp_2.email = 'Test.User@company.com'
-emp_2.pay = 60000
+# print(emp_1)
+# print(emp_2)
 
 print(emp_1.email)
 print(emp_2.email)
