27f262caf7ba6586e0e645da868736f855f38f7e
George Kaimakis <geoptus@gmail.com>
2018-02-12 21:30:35 +0000

    raise_amount as class variable and called with self

--- oop.py
+++ oop.py
@@ -5,6 +5,8 @@ class Employee:
 
     version = '0.1'
 
+    raise_amount = 1.04
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
@@ -15,11 +17,11 @@ class Employee:
         return '{} {}'.format(self.first, self.last)
 
     def apply_raise(self):
-        self.pay = int(self.pay * 1.04)
+        self.pay = int(self.pay * self.raise_amount)
 
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'User', 60000)
 
-print(emp_1.pay)
-emp_1.apply_raise()
-print(emp_1.pay)
+print(Employee.raise_amount)
+print(emp_1.raise_amount)
+print(emp_2.raise_amount)
