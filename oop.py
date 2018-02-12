25434b794bf982cca730ebe58fbebb327118d084
George Kaimakis <geoptus@gmail.com>
2018-02-12 21:20:52 +0000

    hard-code apply_raise method

--- oop.py
+++ oop.py
@@ -14,6 +14,12 @@ class Employee:
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
+    def apply_raise(self):
+        self.pay = int(self.pay * 1.04)
+
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'User', 60000)
 
+print(emp_1.pay)
+emp_1.apply_raise()
+print(emp_1.pay)
