c8c38b18ba3af2b1302ca0998d1fe50c17d6dc5a
George Kaimakis <geoptus@gmail.com>
2018-02-12 21:35:18 +0000

    sets an instance raise_amount separate to class

--- oop.py
+++ oop.py
@@ -22,6 +22,8 @@ class Employee:
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'User', 60000)
 
+emp_1.raise_amount = 1.05
+
 print(Employee.raise_amount)
 print(emp_1.raise_amount)
 print(emp_2.raise_amount)
